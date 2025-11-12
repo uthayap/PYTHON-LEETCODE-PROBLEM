b14499c8b8d8520253776dd0aa16a571a6f4a7ff
uthayap <uthayap.24eie@kongu.edu>
2025-11-12 18:42:55 +0530

    Time: 0 ms (100%), Space: 12.8 MB (88.98%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        counter = 0 
+        for i in nums:
+            if i >= target  :
+                return counter
+            counter += 1
+        return counter
+        
